e430fd17e6c35b7119da504cddf93d436ada2996
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-07-04 01:47:24 -0500

    Transcribe todos reducer function from pre_project2_redux_beginnings

---
+++ src/reducers/todos.js
@@ -0,0 +1,27 @@
+// todos reducer file
+
+// import todo actions from actions folder
+import {
+    ADD_TODO,
+    REMOVE_TODO,
+    TOGGLE_TODO,
+} from '../actions/todos'
+
+// reduces request into a brand new state (must be a pure function)
+// make todos reducer default export
+export default function todos(state = [], action) {
+    switch(action.type){
+        case ADD_TODO:
+            return state.concat([action.todo])
+        case REMOVE_TODO:
+            return state.filter((todo) => todo.id !== action.id)
+        case TOGGLE_TODO:
+            return state.map((todo) => todo.id === action.id
+                ? Object.assign({}, todo, {complete: !todo.complete} )
+                : todo)
+        case RECEIVE_DATA:
+            return action.todos
+        default:
+            return state
+    }
+}
