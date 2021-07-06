3b4d9ec181be244afefafe7c00c9c95d681525ef
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-07-06 00:17:37 -0500

    Update src/index.js to use react-redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+//By importing reducer in this manner, the reducer
+//will be whatever the file './reduceres/'exports!
+//In this case, it will be the invocation of combineReducers
+import reducer from './reducers'
+import middleware from './middleware'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+
+//create store
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
